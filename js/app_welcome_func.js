1a2678e5915be30800175516a597a3ecc503aa86
Sergei <patrushev.s.job@gmail.com>
2026-07-09 16:42:40 -0400

    test

--- js/app_welcome_func.js
+++ js/app_welcome_func.js
@@ -13,11 +13,7 @@ const chatInput =
 ========================= */
 
 const titleText =
-  "Hello! I’m Sergei’s portfolio assistant. I use an LLM enhanced with Retrieval‑Augmented Generation to provide reliable, context‑aware answers about my work and experience." 
-// const titleText =
-//   "Hi, I'm Sergei — a programmer and software developer. This project is both my portfolio and personal AI assistant, created to showcase my skills and help with everyday tasks. Feel free to ask anything — it will do its best to assist you. Enjoy exploring!";
-
-/* in to div */
+  "Hello! I’m iishka from Irishki, ai assistant." 
 
 app_welcome.textContent =
   titleText;
