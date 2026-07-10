17dc3f5bf13c1869278d65574a8f95a2b64216a3
SeJay134 <patrushev.s.job@gmail.com>
2026-07-10 06:58:32 -0400

    json

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import logging
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-
+import json
 from openai import OpenAI
 
 # ----------------- setup -----------------
